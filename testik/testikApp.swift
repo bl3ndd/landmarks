0e9b06c780f9f32946d3f7c5f0b011e294898193
bl3ndd <evarzin@viasattech.ru>
2023-10-22 15:39:35 +0600

    Initial Commit

---
+++ testik/testikApp.swift
@@ -0,0 +1,17 @@
+//
+//  testikApp.swift
+//  testik
+//
+//  Created by Evgeny Varzin on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct testikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
